cb27024a0cb173b605530c3a3ae715009a8b26e2
Antoine Barré <antoine.barre@gmail.com>
2024-07-03 16:06:37 +0200

    feat: clean and add setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='yggdrasil',
+      version='0.1',
+      description='Python Distribution Utilities',
+      author='Antoine Barré',
+      author_email='antoine.barre@gmail.com',
+      packages=['yggdrasil'],
+     )
